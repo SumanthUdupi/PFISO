a7f9da6480e860187eeca511628710650298ade7
Sumanth Udupi <109497622+SumanthUdupi@users.noreply.github.com>
2026-01-14 12:10:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PFISO/', // Set base path for GitHub Pages repo
+  base: 'https://sumanthudupi.github.io/PFISO/', // Set base path for GitHub Pages repo
   build: {
     outDir: 'dist',
     emptyOutDir: true,
